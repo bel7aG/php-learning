fdaeb409a0d4f52cea161b93b2c535b47eb2659d
bel7aG <belhassen.gharsallah@isetrades.com>
2018-10-28 18:55:23 +0100

    play with array in php i feel its so bad compare with JS xD

--- Learn-basics/03-arrays/basic/array-methods.php
+++ Learn-basics/03-arrays/basic/array-methods.php
@@ -29,6 +29,8 @@
       "sass / scss"
     );
 
+    #array_merge(arrayOne, arrayTwo, ...);
+
     $bel7aG = array_merge($myInfo, $mySkills);
 
     echo("<br /><br /><br />");
@@ -37,10 +39,53 @@
       echo $key . ": " . $data . "<br />";
     }
 
+    #array_sum(array);
     $arrayOfSum = array(1, 2, 3, 4, 5, "3", "3", function() {return 11;}, "bel7aG");
 
     $sum = array_sum($arrayOfSum);
     echo $sum;
+
+    #array_keys(array);
+
+    $v = array_keys($myInfo);
+
+    echo "<br />" . is_array($v); //true
+
+    foreach ($v as $keysValue) {
+      $stream = is_integer($keysValue) ? ($keysValue ? "true" : "false") : "$keysValue";
+      echo "<br />" . $stream;
+    }
+
+    #array_fill($stratIndex, numberOfItem, mixed value);
+    $fillArray = array_fill(10, 3, "bel7aG");
+
+    foreach($fillArray as $key => $data) {
+      echo "<br />" . $key . "==> " . $data;
+    }
+
+
+    $arrayOfSum = array_filter($arrayOfSum, function($element) {
+      return is_integer($element);
+    });
+
+    $arrayOfSum = array_map(function($element) {
+      return $element * 2000;
+    }, $arrayOfSum);
+
+    
+    echo "<br /><br />";
+
+    foreach ($arrayOfSum as $key => $data) {
+      echo $key . "==> " . $data . "  |  ";
+    }
+
+    array_push($arrayOfSum, "bel7aG", "github", "javascript");
+    echo "<br /><br /><br />";
+    foreach ($arrayOfSum as $key => $data) {
+      echo $key . "==> " . $data . "  |  ";
+    }
+
+
   ?>
 </body>
 </html>
