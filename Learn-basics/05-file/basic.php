70d05099c9d120dce2d9da4caf13461870a59179
bel7aG <belhassen.gharsallah@isetrades.com>
2018-10-31 15:05:01 +0100

    play with file

--- Learn-basics/05-file/basic.php
+++ Learn-basics/05-file/basic.php
@@ -8,11 +8,63 @@
 </head>
 <body>
   <?php
+    $name = $surname = $email = $age = "";
+    $nameErr = $surnameErr = $emailErr = $ageErr = "";
+
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      if (empty($_POST["name"])) {
+        $nameErr = "name reqiured";
+      } else {
+        $name = $_POST["name"];
+      }
+      if (empty($_POST["surname"])) {
+        $surnameErr = "surname reqiured";
+      } else {
+        $surname = $_POST["surname"];
+      }
+      if (empty($_POST["email"])) {
+        $emailErr = "email reqiured";
+      } else {
+        $email = $_POST["email"];
+      }
+      if (empty($_POST["age"])) {
+        $ageErr = "age reqiured";
+      } else {
+        $age = $_POST["age"];
+      }
+    }
+
+    function isFine($data) {
+      $data = trim($data);
+      $data = htmlspecialchars($data);
+      $data = stripslashes($data);
+      return $data;
+    }
+
+
+  ?>
+
+  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+    name:
+    <input type="text" name="name">
+    surname:
+    <input type="text" name="surname">
+    email:
+    <input type="email" name="email">
+    age:
+    <input type="number" name="age">
+    <button type="submit">GO</button>
+  </form>
+
+
+  <?php
     // r(read onlly), r+ mayékhdmou kén ki yabda fama file makenesh throw an error.
     // $fileTwo = fopen("basicFileTwo.txt", "r+");
     // fwrite($fileTwo, "Belhassen Gharsallah oh yeah" . "\n");
 
-    //w(write only), w+ ezouz ihotou el pointeur fel beginning wi ifaskhouh kén él fichier mch maoujoud yasen3ouh.
+    //w(write only), w+ ezouz ihotou el pointeur fel beginning wi ifaskhouh él contents ou kén
+    //él fichier mch maoujoud yasen3ouh.
     $fileOne = fopen("basicFileOne.txt", "w");
     fwrite($fileOne, "bel7aG Learn file handling inside php file - 1" . "\n");
     fwrite($fileOne, "line two");
@@ -21,9 +73,14 @@
     // thot el pointeur felekher (append), 3lé fékra write tfasakh él contents jémla xD..
     // ou tasna3 él fichier law kénou mch maoujoud kima él w
 
-    $fileThree = fopen("basicThree.txt", "a");
-    fwrite($fileThree, "Belhassen Gharsallah" . "\n");
-    
+    //read the file and get the contents in an array
+    $openFileOne = fopen("basicFileOne.txt", "a");
+    $fileThreeArray = file("basicFileOne.txt");
+
+    foreach($fileThreeArray as $line) {
+      echo $line . "<br /><br /><br />";
+    }
+
 
 
 
@@ -47,7 +104,12 @@
     If the file does not exist, attempt to create it. In this mode,
     fseek() only affects the reading position, writes are always appended.
 
-    -'x'	Create and open for writing only; place the file pointer at the beginning of the file. If the file already exists, the fopen() call will fail by returning FALSE and generating an error of level E_WARNING. If the file does not exist, attempt to create it. This is equivalent to specifying O_EXCL|O_CREAT flags for the underlying open(2) system call.
+    -'x'	Create and open for writing only; place the file pointer at the beginning of the file.
+          If the file already exists, the fopen() call will fail by returning FALSE and
+          generating an error of level E_WARNING. If the file does not exist,
+          attempt to create it.
+          This is equivalent to specifying O_EXCL|O_CREAT flags for the underlying open(2)
+          system call.
     -'x+'	Create and open for reading and writing; otherwise it has the same behavior as 'x'.
 
     -'c'	Open the file for writing only. If the file does not exist, it is created. If it exists,
@@ -62,6 +124,19 @@
     -'e'	Set close-on-exec flag on the opened file descriptor. Only available in PHP compiled on POSIX.1-2008 conform systems.
     */
 
+  ?>
+
+  <?php
+    $myMessage = ($name && $surname && $email && $age) ? (
+      "name: " . $name
+      . "<br />surname: "
+      . $surname
+      . "<br />email: "
+      . $email
+      . "<br />age: "
+      . $age
+    ) : "put your Information correctly please";
+    echo $myMessage;
   ?>
 </body>
 </html>
